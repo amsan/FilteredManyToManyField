421cd915ba48a7bd22ee82b860581dd1d128630b
António Maltsev Santos <antonio.maltsev@gmail.com>
2019-08-18 18:34:53 +0100

    initial models and fixtures

--- customfields/filtered_many_to_many/models.py
+++ customfields/filtered_many_to_many/models.py
@@ -1,4 +1,51 @@
+import uuid
+
 from django.db import models
 
 
-# Create your models here.
+class Person(models.Model):
+    id = models.UUIDField("id", primary_key=True, default=uuid.uuid4)
+    name = models.CharField("name", max_length=255)
+
+    pets = models.ManyToManyField(
+        "Pet",
+        verbose_name="pets",
+        related_name="people",
+        through="PersonPets",
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Pet(models.Model):
+    id = models.UUIDField("id", primary_key=True, default=uuid.uuid4)
+    name = models.CharField("name", max_length=255)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class PersonPets(models.Model):
+    id = models.UUIDField("id", primary_key=True, default=uuid.uuid4)
+
+    person = models.ForeignKey(
+        Person,
+        verbose_name="person",
+        related_name="pet_relations",
+    )
+    pet = models.ForeignKey(
+        Pet,
+        verbose_name="pet",
+        related_name="person_relations",
+    )
+
+    is_deleted = models.BooleanField("is deleted", default=False)
+
+    def __str__(self) -> str:
+        result = f"{self.person} <-> {self.pet}"
+
+        if self.is_deleted:
+            result = f"(DELETED {result})"
+
+        return result
