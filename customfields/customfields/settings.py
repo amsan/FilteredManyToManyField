4432404a70c3bcc624751b90abf20018a2b80006
António Maltsev Santos <antonio.maltsev@gmail.com>
2019-08-19 12:55:44 +0100

    (sql_config): create_queryset_view_sql_item

--- customfields/customfields/settings.py
+++ customfields/customfields/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     # 'django.contrib.messages',
     # 'django.contrib.staticfiles',
     'filtered_many_to_many',
+    'migrate_sql',
 ]
 
 MIDDLEWARE = [
---
+++ customfields/filtered_many_to_many/sql_config.py
@@ -0,0 +1,33 @@
+from migrate_sql.config import SQLItem
+
+from filtered_many_to_many.models.view import (
+    QuerySetViewModel,
+)
+
+
+def create_queryset_view_sql_item(model: QuerySetViewModel) -> SQLItem:
+    """Creates the SQL for the view based on the given QuerySetViewModel"""
+
+    view_name = model._meta.db_table
+
+    query_sql, query_params = model._meta.queryset.query.sql_with_params()
+
+    return SQLItem(
+        name=f'make_{view_name}',
+        sql=[
+            (
+                f'CREATE VIEW {view_name} AS {query_sql};',
+                query_params,
+            ),
+        ],
+        reverse_sql=[
+            (
+                f'DROP VIEW IF EXISTS {view_name};',
+                None,
+            ),
+        ],
+    )
+
+
+sql_items = [
+]
